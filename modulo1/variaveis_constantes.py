fb56b83da87fcfb1894f1a77a06ba79500292906
augustocesarcruz <augustobazilio@gmail.com>
2024-05-24 20:37:47 -0300

    first commit

---
+++ modulo1/variaveis_constantes.py
@@ -0,0 +1,13 @@
+nome = 'Guto'
+idade = 27
+
+nome = 'Arlo'
+
+print(nome, idade)
+
+limite_saque_diario = 1000
+print(limite_saque_diario)
+
+STATES_BRAZIL = ['RJ', 'SP', 'SC', 'RS']
+
+print(STATES_BRAZIL)
---
+++ modulo2/operadores_aritmeticos.py
@@ -0,0 +1,10 @@
+produto_1 = 20
+produto_2 = 10
+
+print(produto_1 + produto_2) #adição
+print(produto_1 - produto_2) #subtração
+print(produto_1 * produto_2) #multiplicação
+print(produto_1 // produto_2) #divisão numero inteiro
+print(produto_1 / produto_2) #divisão float
+print(produto_1 ** produto_2) #resto divisão
+print(produto_1 % produto_2) #exponenciação
