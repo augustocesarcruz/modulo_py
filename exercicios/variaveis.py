e736f25974ab04485dfa401365f761f09436da8f
augustocesarcruz <augustobazilio@gmail.com>
2024-05-24 23:23:26 -0300

    first commit

---
+++ exercicios/variaveis.py
@@ -0,0 +1,12 @@
+# Exercício
+# Crie um programa para armazenar as informações de um livro em variáveis, precisamos armazenar as seguintes informações: título, autor, ano de publicação, número de páginas e editora.
+# Altere o programa do exercício anterior, e adicione uma constante para o número de páginas do livro.
+
+
+titulo = 'Na minha pele'
+autor = 'Lazaro Ramos'
+ano_de_publicacao = 2017
+NUMERO_DE_PAGINAS = 200
+editora = 'Amazon'
+
+print(f'O autor {autor} traz seu novo livro intitulado {titulo} e postado pela {editora} no ano de {ano_de_publicacao}. Ao todo o livro tem {NUMERO_DE_PAGINAS} paginas.')
