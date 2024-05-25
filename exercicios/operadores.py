e3aed3ada383ddc5d3c4c4acd8bab5e864538188
augustocesarcruz <augustobazilio@gmail.com>
2024-05-24 23:48:03 -0300

    first commit

---
+++ exercicios/operadores.py
@@ -0,0 +1,12 @@
+#Exercícios
+#1 - Crie um programa que imprima o valor do maior número entre duas variáveis.
+#2 - Crie um programa que imprima o maior e o menor valor entre três variáveis.
+
+a = 20
+b = 11
+
+if a > b:
+    print('A é maior que B')
+
+if a > b or a > 100:
+    print('A é maior que B e menor que 100')
